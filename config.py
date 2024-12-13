6f09c5ba3fe0e03eb3ca1cdfc1e523d5c2dea41a
Damian Savage <savage.damian@outlook.com>
2024-12-12 21:07:52 -0600

    added more backend

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import certifi
 con_string = "mongodb+srv://test:test@fsdi-107.no6nt.mongodb.net/?retryWrites=true&w=majority&appName=FSDI-107"
 
 client = pymongo.MongoClient(con_string, tlsCAFile=certifi.where())
-db = client.get_database("organika")
+db = client.get_database("dbCh52")
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import json
 from config import db
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # warning this disables CORS policy
 
 @app.get('/')
 def home():
@@ -44,71 +46,79 @@ def fix_id(obj):
     obj["_id"] = str(obj["_id"])
     return obj
 
-@app.get('/api/products')
+@app.get("/api/products")
 def get_products():
     products_db = []
     cursor = db.products.find({})
     for prod in cursor:
-        products_db.append(fix_id(prod))
+        if "title" in prod:
+            products_db.append(fix_id(prod))
     return json.dumps(products_db)
 
-@app.post('/api/products')
+@app.post("/api/products")
 def save_products():
     item = request.get_json()
     print(item)
-    #products.append(item)
+    # products.append(item)
     db.products.insert_one(item)
     return json.dumps(fix_id(item))
 
-#@app.put("/api/products/<int:index>")
-#def update_products(index):
-#    updated_item = request.get_json()
-#    if 0<= index <= len(products):
-#        products[index] = updated_item
-#        return json.dumps(updated_item)
-#    else:
-#        return "That index does not exist"
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_item = request.get_json()
+    if 0 <= index <= len(products):
+        products[index] = updated_item
+        return json.dumps(updated_item)
+    else:
+        return "That index does not exist"
 
-#@app.delete('/api/products/<int:index>')
-#def delete_product(index):
-#    delete_item = request.get_json()
-#    if 0<= index <= len(products):
-#        delete_item = products.pop(index)
-#        return json.dumps(delete_item)
-#    else:
-#        return "That index does not exist"
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    delete_item = request.get_json()
+    if 0 <= index <= len(products):
+        delete_item = products.pop(index)
+        return json.dumps(delete_item)
+    else:
+        return "That index does not exist"
 
+# pacth -- the method to update an especific element into python is: list.update
 
-#@app.patch('/api/products/<int:index>')
-#def patch_products(index):
-#    updated_field = request.get_json()
-#    if 0<=index<=len(products):
-#        updated_field(index).update(updated_field)
-#        return json.dumps(updated_field)
-#    else:
-#        return "That index does not exist"
+@app.patch("/api/products/<int:index>")
+def patch_products(index):
+    updated_field = request.get_json()
+    if 0 <= index <= len(products):
+        updated_field(index).update(updated_field)
+        return json.dumps(updated_field)
+    else:
+        return "That index does not exist"
+    
+#######################################
+############ COUPONS ##################
+#######################################
 
-# Final Report
-products = []
-
-def fix_id(obj):
-    obj["_id"] = str(obj["_id"])
-    return obj
-
-@app.get('/api/catalog')
-def get_objects():
-    products_db = []
-    cursor = db.products.find({})
-    for prod in cursor:
-        products_db.append(fix_id(prod))
-    return json.dumps(products_db)
-
-@app.post('/api/products')
-def save_products():
+@app.post("/api/coupons")
+def save_coupons():
     item = request.get_json()
-    print(item)
-    #products.append(item)
-    db.products.insert_one(item)
+    db.coupons.insert_one(item)
     return json.dumps(fix_id(item))
+
+@app.get("/api/coupons")
+def get_coupons():
+    coupons_db = []
+    cursor = db.coupons.find({})
+    for coupon in cursor:
+        coupons_db.append(fix_id(coupon))
+        
+    return json.dumps(coupons_db)
+
+@app.get("/api/coupons/<code>")
+def validate_coupon(code):
+    coupon = db.coupons.find_one({"code": code})
+    if coupon == None:
+        print("Error: Invalid Coupon")
+        return abort(404, "Invalid Code")
+        
+    return json.dumps(fix_id(coupon))
+
 
 app.run(debug=True)
